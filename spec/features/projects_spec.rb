33c7adfdec73e0488a76f809060cfa234bdb3f85
Grace <gneumaye@uccs.edu>
2021-04-24 03:35:45 +0000

    edited Rspec testing and simplecov

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.feature "Projects", type: :feature do
   pending "add some scenarios (or delete) #{__FILE__}"
+end
 
+RSpec.feature "Projects", type: :feature do
   context "Create new project" do
     before(:each) do
       visit new_project_path
